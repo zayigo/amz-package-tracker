acb7640154755a9405efdbf538555e945174dac9
zayigo <me@nicola.tn.it>
2024-11-22 10:49:50 +0100

    Fix FBA csrf-token retrieval

--- pageScript.js
+++ pageScript.js
@@ -2,7 +2,11 @@
   window.postMessage(
     {
       type: 'FROM_PAGE_SCRIPT',
-      csrfToken: window.csrfToken,
+      csrfToken:
+        window.csrfToken ||
+        document
+          .querySelector('meta[name="CSRF-TOKEN"]')
+          ?.getAttribute('content'),
     },
     '*'
   );
